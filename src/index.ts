66407ccbce3a3251d758aae13358bc098cc197e6
SuzukiDaishi <zukky.rikugame@gmail.com>
2022-04-09 12:09:14 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -20,10 +20,12 @@ window.addEventListener('DOMContentLoaded', async () => {
   document.body.appendChild(renderer.domElement)
 
   const scene = new THREE.Scene()
-  const camera = new THREE.PerspectiveCamera(
-    45,
-    window.innerWidth/window.innerHeight,
-    0.1,
+  const camera = new THREE.OrthographicCamera(
+    -480, 
+    +480, 
+    +270, 
+    -270,
+    0.1, 
     1000
   )
 
